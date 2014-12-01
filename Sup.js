655018c8a210ae5375d079dd6fafb2352247119a
Vitor Augusto da Silva Vasconcellos <vasvas10@gmail.com>
2014-12-01 00:26:21 -0200

    no message

--- Sup.js
+++ Sup.js
@@ -782,6 +782,7 @@ Sup.prototype.sendMessage = function sendTextMessage(to, text, callback){
         self._writeMsg('text', {text: text, to: to}, true, callback);
     }else {
         setImmediate(function () {
+            console.log('teste');
             self.sendMessage(to, text, callback);
         });
     }
